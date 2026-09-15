d1616b669b9db7d803eeea372fcbff89b024d5ac
Xabier Giron Hermo <xabier.giron@gmail.com>
2026-09-15 13:35:42 +0200

    commit marets 15 sin corregir

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import event, Create3
+import asyncio
 
 # Inicializa el robot Create 3 por Bluetooth
 robot = Create3(Bluetooth("C3_UIEC_Grupo2"))
@@ -8,8 +9,15 @@ robot = Create3(Bluetooth("C3_UIEC_Grupo2"))
 @event(robot.when_play)
 async def play(robot):
     print('Conectado al robot')
-    # Comando para que el robot avance 10 cm
-    await robot.move(10)
+    
+    # Fijar luz roja
+    await robot.set_lights_on_rgb(255, 0, 0)
+    
+    # Ruido de Desconexión
+    await robot.play_note(440, 1)
+    
+    # Comando para que el robot se desplace -20cm hacia atrás
+    await robot.move(-20)
 
 # Inicia y mantiene activo el motor de eventos del robot
 robot.play()
